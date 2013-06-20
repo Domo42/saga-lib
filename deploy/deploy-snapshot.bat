b7ca31dd8eb3311fab68ce9de5a2404ce5f52139
Domo <stefan@codebullets.com>
2013-06-20 22:13:49 +0200

    Set version of poms during deploy.

--- deploy/deploy-snapshot.bat
+++ deploy/deploy-snapshot.bat
@@ -1,7 +1,17 @@
 SET targetVersion=0.1-SNAPSHOT
 SET baseDir=%~dp0
 
-call mvn deploy:deploy-file -Durl=https://oss.sonatype.org/content/repositories/snapshots ^
+REM change version number in project tree
+CD ..
+CALL mvn versions:set -DgenerateBackupPoms=false -DnewVersion=%targetVersion%
+
+
+REM change version numbers on artifact deployment poms
+CD deploy
+CALL mvn --file saga-lib.pom.xml versions:set -DgenerateBackupPoms=false -DnewVersion=%targetVersion%
+
+
+CALL mvn deploy:deploy-file -Durl=https://oss.sonatype.org/content/repositories/snapshots ^
                             -DrepositoryId=sonatype-nexus-snapshots ^
                             -Dfile=%baseDir%\..\saga-lib\target\saga-lib-%targetVersion%.jar ^
                             -DpomFile=%baseDir%\saga-lib.pom.xml ^
--- deploy/deploy-staging.bat
+++ deploy/deploy-staging.bat
@@ -2,6 +2,15 @@
 SET targetVersion=0.1
 SET baseDir=%~dp0
 
+REM change version number in project tree
+CD ..
+CALL mvn versions:set -DgenerateBackupPoms=false -DnewVersion=%targetVersion%
+
+
+REM change version numbers on artifact deployment poms
+CD deploy
+CALL mvn --file saga-lib.pom.xml versions:set -DgenerateBackupPoms=false -DnewVersion=%targetVersion%
+
 gpg -u AA9AEC3C --sign --detach-sign -a saga-lib.pom.xml
 
 REM deploy base artifacts
@@ -27,3 +36,13 @@ call mvn deploy:deploy-file -Durl=https://oss.sonatype.org/service/local/staging
                             -DrepositoryId=sonatype-nexus-staging ^
                             -Dfile=%baseDir%\..\saga-lib\target\saga-lib-%targetVersion%-sources.jar.asc ^
                             -DpomFile=%baseDir%\saga-lib.pom.xml
+
+GOTO:eof
+
+:error
+    ECHO.
+    ECHO -----------------------------------
+    ECHO - Error performing project staging
+    ECHO -----------------------------------
+    ECHO.
+    EXIT /B
