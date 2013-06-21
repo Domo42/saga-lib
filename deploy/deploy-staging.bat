3f8e5fbb422e8b45dd5a3120b4c756ed512a28ca
Domo <stefan@codebullets.com>
2013-06-21 20:38:44 +0200

    Stage first release.

--- deploy/deploy-staging.bat
+++ deploy/deploy-staging.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
 SET targetVersion=0.1
 SET baseDir=%~dp0
+SET targetUrl=https://oss.sonatype.org/service/local/staging/deploy/maven2
 
 REM change version number in project tree
 CD ..
@@ -11,31 +12,52 @@ REM change version numbers on artifact deployment poms
 CD deploy
 CALL mvn --file saga-lib.pom.xml versions:set -DgenerateBackupPoms=false -DnewVersion=%targetVersion%
 
-gpg -u AA9AEC3C --sign --detach-sign -a saga-lib.pom.xml
+del *.asc
+
+CD ..
+call mvn clean install -DperformRelease=true -Dgpg.keyname=AA9AEC3C
+
+CD deploy
+SET pomAscFile=saga-lib-%targetVersion%.pom.asc
+
+gpg -u AA9AEC3C --sign --detach-sign -o %pomAscFile% -a saga-lib.pom.xml
+
 
 REM deploy base artifacts
-call mvn deploy:deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy ^
+call mvn deploy:deploy-file -Durl=%targetUrl% ^
                             -DrepositoryId=sonatype-nexus-staging ^
                             -Dfile=%baseDir%\..\saga-lib\target\saga-lib-%targetVersion%.jar ^
                             -DpomFile=%baseDir%\saga-lib.pom.xml ^
                             -Djavadoc=%baseDir%\..\saga-lib\target\saga-lib-%targetVersion%-javadoc.jar ^
                             -Dsources=%baseDir%\..\saga-lib\target\saga-lib-%targetVersion%-sources.jar
+                            -Dfiles
 
 REM deploy signatures
-call mvn deploy:deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy ^
+call mvn deploy:deploy-file -Durl=%targetUrl% ^
+                            -DrepositoryId=sonatype-nexus-staging ^
+                            -Dfile=%baseDir%\%pomAscFile% ^
+                            -DpomFile=%baseDir%\saga-lib.pom.xml ^
+                            -Dpackaging=pom.asc
+
+call mvn deploy:deploy-file -Durl=%targetUrl% ^
                             -DrepositoryId=sonatype-nexus-staging ^
                             -Dfile=%baseDir%\..\saga-lib\target\saga-lib-%targetVersion%.jar.asc ^
-                            -DpomFile=%baseDir%\saga-lib.pom.xml
+                            -DpomFile=%baseDir%\saga-lib.pom.xml ^
+                            -Dpackaging=jar.asc
 
-call mvn deploy:deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy ^
+call mvn deploy:deploy-file -Durl=%targetUrl% ^
                             -DrepositoryId=sonatype-nexus-staging ^
                             -Dfile=%baseDir%\..\saga-lib\target\saga-lib-%targetVersion%-javadoc.jar.asc ^
-                            -DpomFile=%baseDir%\saga-lib.pom.xml
+                            -DpomFile=%baseDir%\saga-lib.pom.xml ^
+                            -Dclassifier=javadoc ^
+                            -Dpackaging=jar.asc
 
-call mvn deploy:deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy ^
+call mvn deploy:deploy-file -Durl=%targetUrl% ^
                             -DrepositoryId=sonatype-nexus-staging ^
                             -Dfile=%baseDir%\..\saga-lib\target\saga-lib-%targetVersion%-sources.jar.asc ^
-                            -DpomFile=%baseDir%\saga-lib.pom.xml
+                            -DpomFile=%baseDir%\saga-lib.pom.xml ^
+                            -Dclassifier=sources ^
+                            -Dpackaging=jar.asc
 
 GOTO:eof
 
