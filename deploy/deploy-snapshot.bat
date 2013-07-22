d0e385e11b0c72d963df7a35c8c53da556f1b2e0
Domo42 <stefan@codebullets.com>
2013-07-22 10:05:46 +0200

    create snapshot version

--- deploy/deploy-snapshot.bat
+++ deploy/deploy-snapshot.bat
@@ -1,4 +1,4 @@
-SET targetVersion=0.3-SNAPSHOT
+SET targetVersion=0.5-SNAPSHOT
 SET baseDir=%~dp0
 
 REM change version number in project tree
@@ -9,15 +9,15 @@ CALL mvn versions:set -DgenerateBackupPoms=false -DnewVersion=%targetVersion%
 REM change version numbers on artifact deployment poms
 CD deploy
 CALL mvn --file saga-lib.pom.xml versions:set -DgenerateBackupPoms=false -DnewVersion=%targetVersion%
+CALL mvn --file saga-lib-guice.pom.xml versions:set -DgenerateBackupPoms=false -DnewVersion=%targetVersion%
 
-
+REM deploy artifacts
 CALL mvn deploy:deploy-file -Durl=https://oss.sonatype.org/content/repositories/snapshots ^
                             -DrepositoryId=sonatype-nexus-snapshots ^
                             -Dfile=%baseDir%\..\saga-lib\target\saga-lib-%targetVersion%.jar ^
                             -DpomFile=%baseDir%\saga-lib.pom.xml ^
                             -Djavadoc=%baseDir%\..\saga-lib\target\saga-lib-%targetVersion%-javadoc.jar ^
                             -Dsources=%baseDir%\..\saga-lib\target\saga-lib-%targetVersion%-sources.jar
-
 
 CALL mvn deploy:deploy-file -Durl=https://oss.sonatype.org/content/repositories/snapshots ^
                             -DrepositoryId=sonatype-nexus-snapshots ^
