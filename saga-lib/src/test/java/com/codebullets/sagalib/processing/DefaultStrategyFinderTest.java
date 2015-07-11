a7e7d3114b1c62e9e5ead50880487b3f86b80945
Domo42 <stefan@codebullets.com>
2015-07-11 20:51:29 +0200

    more tests

---
+++ saga-lib/src/test/java/com/codebullets/sagalib/processing/DefaultStrategyFinderTest.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2015 Stefan Domnanovits
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.codebullets.sagalib.processing;
+
+import com.codebullets.sagalib.TestSaga;
+import com.codebullets.sagalib.context.LookupContext;
+import com.codebullets.sagalib.storage.StateStorage;
+import com.codebullets.sagalib.timeout.Timeout;
+import com.google.common.collect.Lists;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collection;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.isA;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Test for {@link DefaultStrategyFinder} class.
+ */
+public class DefaultStrategyFinderTest {
+    private DefaultStrategyFinder sut;
+    private TypesForMessageMapper messageMapper;
+
+    @Before
+    public void init() {
+        SagaInstanceFactory instanceFactory = mock(SagaInstanceFactory.class);
+        KeyExtractor keyExtractor = mock(KeyExtractor.class);
+        StateStorage storage = mock(StateStorage.class);
+        messageMapper = mock(TypesForMessageMapper.class);
+
+        sut = new DefaultStrategyFinder(messageMapper, instanceFactory, keyExtractor, storage);
+    }
+
+    /**
+     * <pre>
+     * Given => Timeout message parameter
+     * When  => find is called
+     * Then  => returns a timeout strategy
+     * </pre>
+     */
+    @Test
+    public void find_timeout_returnsTimeoutStrategy() {
+        // given
+        Timeout timeout = Timeout.create(null, null, null, null);
+        LookupContext context = mockMessageContext(timeout);
+
+        // when
+        Collection<ResolveStrategy> strategies = sut.find(context);
+
+        // then
+        assertThat("Expected a timeout strategy.", strategies, hasItem(isA(TimeoutResolveStrategy.class)));
+    }
+
+    /**
+     * <pre>
+     * Given => message to create a new instance is called
+     * When  => find is called
+     * Then  => returns a start new saga strategy
+     * </pre>
+     */
+    @Test
+    public void find_newInstanceMessage_returnsStartNewSagaStrategy() {
+        // given
+        Object message = mockNewSagaMessage();
+        LookupContext context = mockMessageContext(message);
+
+        // when
+        Collection<ResolveStrategy> strategies = sut.find(context);
+
+        // then
+        assertThat("Expected a new saga strategy.", strategies, hasItem(isA(StartNewSagaStrategy.class)));
+    }
+
+    /**
+     * <pre>
+     * Given => message to continue an existing saga
+     * When  => find is called
+     * Then  => returns a continue saga strategy
+     * </pre>
+     */
+    @Test
+    public void find_continueSagaMessage_returnsContinueSagaStrategy() {
+        // given
+        Object message = mockContinueMessage();
+        LookupContext context = mockMessageContext(message);
+
+        // when
+        Collection<ResolveStrategy> strategies = sut.find(context);
+
+        // then
+        assertThat("Expected a continue saga strategy.", strategies, hasItem(isA(ContinueSagaStrategy.class)));
+    }
+
+    private Object mockNewSagaMessage() {
+        Object message = new Object();
+        SagaType sagaType = SagaType.startsNewSaga(TestSaga.class);
+
+        when(messageMapper.getSagasForMessageType(message.getClass())).thenReturn(Lists.newArrayList(sagaType));
+        return message;
+    }
+
+    private LookupContext mockMessageContext(final Object message) {
+        LookupContext context = mock(LookupContext.class);
+        when(context.message()).thenReturn(message);
+
+        return context;
+    }
+
+    private Object mockContinueMessage() {
+        Object message = new Object();
+        SagaType sagaType = SagaType.continueSaga(TestSaga.class);
+
+        when(messageMapper.getSagasForMessageType(message.getClass())).thenReturn(Lists.newArrayList(sagaType));
+        return message;
+    }
+}
---
+++ saga-lib/src/test/java/com/codebullets/sagalib/processing/StrategyInstanceResolverTest.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2015 Stefan Domnanovits
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.codebullets.sagalib.processing;
+
+import com.codebullets.sagalib.context.LookupContext;
+import com.google.common.collect.Lists;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Tests for {@link StrategyInstanceResolver} class.
+ */
+public class StrategyInstanceResolverTest {
+    private StrategyInstanceResolver sut;
+
+    private StrategyFinder strategyFinder;
+
+    @Before
+    public void init() {
+        strategyFinder = mock(StrategyFinder.class);
+        sut = new StrategyInstanceResolver(strategyFinder);
+    }
+
+    /**
+     * <pre>
+     * Given => Finder returns a given strategy.
+     * When  => resolve is called
+     * Then  => call resolve on strategy
+     * </pre>
+     */
+    @Test
+    public void resolve_finderReturnsStrategy_callResolveOnStrategy() {
+        // given
+        LookupContext context = mock(LookupContext.class);
+        ResolveStrategy strategy = mockStrategy(context);
+
+        // when
+        sut.resolve(context);
+
+        // then
+        verify(strategy).resolve(context);
+    }
+
+    /**
+     * <pre>
+     * Given => strategy returns an instance
+     * When  => resolve is called
+     * Then  => resolve return value contains instance
+     * </pre>
+     */
+    @Test
+    public void resolve_strategyReturnsInstance_instanceReturnedWithReturnValue() {
+        // given
+        ResolveStrategy strategy = mockStrategy();
+        SagaInstanceInfo instanceInfo = new SagaInstanceInfo(null, true);
+        when(strategy.resolve(any(LookupContext.class))).thenReturn(Lists.newArrayList(instanceInfo));
+
+        // when
+        Collection<SagaInstanceInfo> instances = sut.resolve(null);
+
+        // then
+        assertThat("Expected instance in result set.", instances, hasItem(instanceInfo));
+    }
+
+    /**
+     * <pre>
+     * Given => strategy returns empty result set
+     * When  => resolve is called
+     * Then  => returns list is empty
+     * </pre>
+     */
+    @Test
+    public void resolve_strategyReturnsEmptyList_resolveReturnListIsEmpty() {
+        // given
+        ResolveStrategy strategy = mockStrategy();
+        when(strategy.resolve(any(LookupContext.class))).thenReturn(Collections.<SagaInstanceInfo>emptyList());
+
+        // when
+        Collection<SagaInstanceInfo> instances = sut.resolve(null);
+
+        // then
+        assertThat("Expected an empty list.", instances, hasSize(0));
+    }
+
+    private ResolveStrategy mockStrategy() {
+        ResolveStrategy strategy = mock(ResolveStrategy.class);
+        when(strategyFinder.find(any(LookupContext.class))).thenReturn(Lists.newArrayList(strategy));
+        return strategy;
+    }
+
+    private ResolveStrategy mockStrategy(final LookupContext context) {
+        ResolveStrategy strategy = mock(ResolveStrategy.class);
+        when(strategyFinder.find(context)).thenReturn(Lists.newArrayList(strategy));
+        return strategy;
+    }
+}
